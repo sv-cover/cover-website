d922edb525fe1251544794320aabb64b0abdce79
Martijn Luinstra <martijnluinstra@gmail.com>
2024-01-09 13:22:02 +0100

    Create polls in public schema

--- data/webcie-minimal.sql
+++ data/webcie-minimal.sql
@@ -1057,7 +1057,7 @@ ALTER TABLE public.stickers OWNER TO webcie;
 --
 -- Polls
 --
-CREATE TABLE polls (
+CREATE TABLE public.polls (
     id serial PRIMARY KEY,
     member_id integer DEFAULT NULL REFERENCES leden (id) ON DELETE SET DEFAULT,
     committee_id integer DEFAULT NULL REFERENCES commissies (id) ON DELETE SET DEFAULT,
@@ -1067,13 +1067,13 @@ CREATE TABLE polls (
     closed_on timestamp without time zone DEFAULT NULL
 );
 
-CREATE TABLE poll_options (
+CREATE TABLE public.poll_options (
     id serial PRIMARY KEY,
     poll_id integer NOT NULL REFERENCES polls (id) ON DELETE CASCADE,
     option character varying(255) NOT NULL
 );
 
-CREATE TABLE poll_votes (
+CREATE TABLE public.poll_votes (
     id serial PRIMARY KEY,
     poll_option_id integer NOT NULL REFERENCES poll_options (id) ON DELETE CASCADE,
     member_id integer DEFAULT NULL REFERENCES leden (id) ON DELETE SET DEFAULT, -- Preserve vote, even if we don't have member
@@ -1081,7 +1081,7 @@ CREATE TABLE poll_votes (
     -- no updated_on, votes cannot be updated
 );
 
-CREATE TABLE poll_comments (
+CREATE TABLE public.poll_comments (
     id serial PRIMARY KEY,
     poll_id integer NOT NULL REFERENCES polls (id) ON DELETE CASCADE,
     member_id integer DEFAULT NULL REFERENCES leden (id) ON DELETE SET DEFAULT,  -- Preserve comment, even if we don't have member
@@ -1090,7 +1090,7 @@ CREATE TABLE poll_comments (
     updated_on timestamp without time zone NOT NULL DEFAULT ('now'::text)::timestamp(6) without time zone
 );
 
-CREATE TABLE poll_likes (
+CREATE TABLE public.poll_likes (
     id serial PRIMARY KEY,
     poll_id integer NOT NULL REFERENCES polls (id) ON DELETE CASCADE,
     member_id integer DEFAULT NULL REFERENCES leden (id) ON DELETE SET DEFAULT, -- Preserve like, even if we don't have member
@@ -1099,7 +1099,7 @@ CREATE TABLE poll_likes (
     CONSTRAINT poll_like_uniq UNIQUE (poll_id, member_id)
 );
 
-CREATE TABLE poll_comment_likes (
+CREATE TABLE public.poll_comment_likes (
     id serial PRIMARY KEY,
     poll_comment_id integer NOT NULL REFERENCES poll_comments (id) ON DELETE CASCADE,
     member_id integer DEFAULT NULL REFERENCES leden (id) ON DELETE SET DEFAULT, -- Preserve like, even if we don't have member
