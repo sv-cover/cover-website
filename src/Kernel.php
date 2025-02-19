78280a212fa5cb7934adc98657fa927b9efc0add
Martijn Luinstra <martijnluinstra@gmail.com>
2025-02-19 23:15:23 +0100

    Revert "Fix timezone?"
    This reverts commit e04cc9006539a5735ea61229921fe1343f365c35.

--- src/Kernel.php
+++ src/Kernel.php
@@ -12,6 +12,12 @@ class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
 
+    public function boot(): void
+    {
+        parent::boot();
+        \date_default_timezone_set($this->getContainer()->getParameter('app.timezone'));
+    }
+
     public function process(ContainerBuilder $container): void
     {
         // Clear tag on DataModel. Unfortunately, this is the only way to make it work.
