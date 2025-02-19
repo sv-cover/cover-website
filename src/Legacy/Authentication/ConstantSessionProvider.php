a77c9adb7c6a302946e6c80169f54fa396105557
Martijn Luinstra <martijnluinstra@gmail.com>
2025-02-19 15:13:21 +0100

    Debug constant session provider

--- src/Legacy/Authentication/ConstantSessionProvider.php
+++ src/Legacy/Authentication/ConstantSessionProvider.php
@@ -12,7 +12,7 @@ class ConstantSessionProvider implements SessionProviderInterface
      */
     private $session;
 
-    public function __construc(DataIterSession $session = null)
+    public function __construct(DataIterSession $session = null)
     {
         $this->session = $session;
     }
