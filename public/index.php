e04cc9006539a5735ea61229921fe1343f365c35
Martijn Luinstra <martijnluinstra@gmail.com>
2025-02-19 23:08:18 +0100

    Fix timezone?

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@ use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Cover >>>>>>>>>>>>>>>
+date_default_timezone_set('Europe/Amsterdam');
+// <<<<<<<<<<<<<<< Cover
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
