1322da232965026b7d530a5ba33c9fe50e6d04cb
Julian Sprietsma <jwsprietsma@gmail.com>
2026-02-21 23:49:38 +0100

    Update to the calendar (now broken)

--- src/Form/Type/CalendarType.php
+++ src/Form/Type/CalendarType.php
@@ -17,10 +17,13 @@ class CalendarType extends AbstractType
     {
         $resolver->setDefaults([
             'TimePerSlot' => 30,
-            'StartHour' => 9,
-            'EndHour' => 17,
+            'startTime' => '09:00',
+            'EndHour' => '17:00',
             'Days' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+            'expanded' => true,
+            'multiple' => true,
         ]);
+
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -32,7 +35,7 @@ class CalendarType extends AbstractType
         {
             for ($j = 0; $j < $nslots; $j++)
             {
-                $builder->add('test' . $j . '-day'  . $i, CheckBoxType::class, [
+                $builder->add('slot' . $j . '-day'  . $i, CheckBoxType::class, [
                     'label' => __(' '),
                 ]);
             }
@@ -41,9 +44,39 @@ class CalendarType extends AbstractType
 
     public function buildView(FormView $view, FormInterface $form, array $options): void
     {
-        $view->vars['Days'] = $options['Days'];
-        $view->vars['Rows'] = (($options['EndHour'] - $options['StartHour']) / ($options['TimePerSlot'] / 60)) - 1;
 
+        $nSlots = ($options['EndHour'] - $options['StartHour']) / ($options['TimePerSlot'] / 60);
+
+        $times = [];
+        for ($i = 0; $i <= $nSlots; $i++)
+        {
+            $hour = ($i != 0 ? $times[$i - 1][0] : $options['StartHour']);
+            $minute = ($i != 0 ? $times[$i - 1][1] : 0);
+
+            if ($i > 0)
+            {
+                $add = $options['TimePerSlot'];
+
+                if (($minute + $add) >= 60)
+                {
+                    $hour += ($minute + $add) / 60;
+                    $minute = ($minute + $add) % 60;
+                } else 
+                {
+                    $minute += $add;
+                }
+            }
+
+            $times[$i] = [$hour, $minute];
+        }
+
+
+        $view->vars['Days'] = $options['Days'];
+        $view->vars['Rows'] = $nSlots;
+        $view->vars['Times'] = $times;
+
+        
+        
     }
 
 }
